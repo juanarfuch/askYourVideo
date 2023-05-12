58003b16e84dd1b2de541497aeddfd081ea554d1
juanarfuch <juan.arfuch@gmail.com>
2023-05-12 06:53:43 -0300

    Adding comments and adjustments

--- utils/database.py
+++ utils/database.py
@@ -2,10 +2,23 @@ import logging
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
 
+# Instantiate OpenAI embeddings
 embeddings = OpenAIEmbeddings()
+
+# Set up logging
 logger = logging.getLogger(__name__)
 
 def create_db(docs, embeddings=embeddings):
+    """
+    Function to create a new FAISS database from the given documents.
+
+    Parameters:
+    docs: A list of documents to be included in the database.
+    embeddings: The embeddings to be used. Default is OpenAI embeddings.
+
+    Returns:
+    The FAISS database, or None if an error occurs or no documents are provided.
+    """
     if not docs:
         logger.error("No documents to create the database.")
         return None
