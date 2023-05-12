623603437e4a264bc23330ae527051489bbb7897
juanarfuch <74027623+juanarfuch@users.noreply.github.com>
2023-05-12 06:34:34 -0300

    Update app.py
    moddifying the chat model, modifying the ui.

--- app.py
+++ app.py
@@ -2,14 +2,15 @@
 import streamlit as st
 from utils.video_processing import load_transcript, split_transcript
 from utils.database import create_db
-from langchain.llms import OpenAI
+from langchain.chat_models import ChatOpenAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.chains import ConversationalRetrievalChain
 from langchain.chains import LLMChain
 from utils.prompts import CONDENSE_PROMPT, QA_PROMPT
+openaiapikey = st.secrets["OPENAI_API_KEY"]
 
 # Set Streamlit page configuration
-st.set_page_config(page_title='🧠ChatBot🤖', layout='wide')
+st.set_page_config(page_title='🧠MAKERS TEST🤖', layout='wide')
 
 # Initialize session states
 if "chat_history" not in st.session_state:
@@ -21,32 +22,26 @@ if "video_loaded" not in st.session_state:
 if "chain" not in st.session_state:
     st.session_state["chain"] = None
 
-# Sidebar options
-with st.sidebar.expander("🛠️ Settings ", expanded=False):
-    # Add some explanation here
-    st.markdown("""To start a new chat, click the button below. This will clear your chat history and allow you to start a new conversation.""")
-    # Option to start a new chat
-    if st.button("New Chat"):
-        st.session_state["chat_history"] = []
-        st.session_state["video_url"] = ""
-        st.session_state["video_loaded"] = False
-        st.session_state["chain"] = None
+# Create a container for the header to make it more accessible
+header = st.container()
 
-# Set up the Streamlit app layout
-st.title("🤖 Chat with your videos --By Juan Arfuch")
-st.markdown(""" 
-       Welcome to my unique ChatBot powered by LangChain, OpenAI, and Streamlit. This is a project for Makers, created with lots of ❤️ and ☕.
-    This bot is not your ordinary bot, it's a super bot that uses the transcript of a YouTube video of your choice to answer your questions. 
-    All you have to do is provide a YouTube video URL, and then you can ask any questions related to the video content.
-    Let's make learning fun and interactive!
-""") 
+with header:
+    st.title("🤖 Chat with your videos --By Juan Arfuch")
+    st.markdown(""" 
+        Welcome to my unique ChatBot powered by LangChain, OpenAI, and Streamlit. This is a project for Makers, created with lots of ❤️ and ☕.
+        This bot is not your ordinary bot, it's a super bot that uses the transcript of a YouTube video of your choice to answer your questions. 
+        All you have to do is provide a YouTube video url includint the id, and then you can ask any questions related to the video content.
+        Let's make learning fun and interactive!
+    """) 
+    # New chat button has been moved to the header for better accessibility
 
 # Wrap transcript loading and splitting in try/except block
 try:
     if not st.session_state["video_loaded"]:
-        st.session_state["video_url"] = st.text_input("Please enter the YouTube video URL: ")
-        if st.button('Load video'):
-            if st.session_state["video_url"]:
+        with st.form('Load video'):
+            st.session_state["video_url"] = st.text_input("Please enter the YouTube video URL: ")
+            submitted = st.form_submit_button("Load Video")
+            if submitted:
                 with st.spinner('Loading video transcript...'):
                     transcript = load_transcript(st.session_state["video_url"])
                     docs = split_transcript(transcript)
@@ -54,33 +49,70 @@ try:
 
                 st.success("Video transcript loaded successfully!")
                 st.session_state["video_loaded"] = True
-
-                llm = OpenAI(temperature=0.2)
-                question_generator = LLMChain(llm=llm, prompt=CONDENSE_PROMPT)
-                doc_chain = load_qa_chain(llm, prompt=QA_PROMPT)
+                ##Creating the conversational retrieval chain###
+                ###Setting the chat model
+                chat= ChatOpenAI(temperature=0.2, model_name="gpt-3.5-turbo")
+                question_generator = LLMChain(llm=chat, prompt=CONDENSE_PROMPT)
+                doc_chain = load_qa_chain(chat, prompt=QA_PROMPT, chain_type="stuff")
                 st.session_state["chain"] = ConversationalRetrievalChain(
                     retriever=db,
                     question_generator=question_generator,
                     combine_docs_chain=doc_chain,
                 )
-        
-    user_question = st.text_input("Enter your question related to the video content")
-    if st.button('Submit question'):
-        if user_question and st.session_state["video_loaded"] and st.session_state["chain"]:
-            with st.spinner('Processing your question...'):
-                result = st.session_state["chain"]({"question": user_question, "chat_history": st.session_state["chat_history"]})
-                st.session_state["chat_history"].append((user_question, result['answer']))
+            else:
+                st.warning("Please provide a valid YouTube URL, make sure you are including the video id")
+    else:
+        st.success("Video transcript is already loaded. You can start asking questions.")
 
-        # Display the conversation history
-        with st.expander("Conversation History", expanded=True):
-            for user, bot in st.session_state["chat_history"]:
-                st.markdown(f'**User**: {user}')
-                st.markdown(f'<span style="color:green">**Bot**: {bot}</span>', unsafe_allow_html=True)
+except Exception as e:
+    st.error(f"An error occurred: {str(e)}. Please try again with a different video URL.")
+
+# Ask questions and display responses
+try: 
+    if st.session_state["chain"] is not None:
+        user_question = st.text_input("Enter your question related to the video content")
+       
+        if st.button('Submit question'):
+            if user_question and st.session_state["video_loaded"] and st.session_state["chain"]:
+                with st.spinner('Processing your question...'):
+                    result = st.session_state["chain"]({"question": user_question, "chat_history": st.session_state["chat_history"]})
+                    st.session_state["chat_history"].append((user_question, result['answer']))
+        
+                # Display the conversation history
+                with st.expander("Conversation History", expanded=True):
+                    for user, bot in st.session_state["chat_history"]:
+                        st.markdown(f'**User**: {user}')
+                        st.markdown(f'<span style="color:green">**Bot**: {bot}</span>', unsafe_allow_html=True)
+        if st.button("Start new chat"):
+            st.session_state["chat_history"] = []
+            st.session_state["video_url"] = ""
+            st.session_state["video_loaded"] = False
+            st.session_state["chain"] = None
+
 except Exception as e:
     st.error(f'An error occurred: {e}')
     st.markdown("""
         Please make sure the YouTube URL is correct and accessible. If the problem persists, try to start a new chat.
     """)
+    with st.form(key='question_form'):
+        st.markdown("Ask a question related to the video content:")
+        question = st.text_input("")
+        submit_button = st.form_submit_button(label='Ask')
+
+        if submit_button:
+            if question:
+                with st.spinner('Generating response...'):
+                   response = st.session_state["chain"]({"question": user_question, "chat_history":st.session_state["chat_history"]})
+                   st.session_state["chat_history"].append({'user': question, 'bot': response})
+            else:
+                st.warning("Please enter a question before pressing the Ask button.")
+
+    # Display chat history
+    if st.session_state["chat_history"]:
+        for chat in st.session_state["chat_history"]:
+            st.markdown(f"**You:** {chat['user']}")
+            st.markdown(f"**ChatBot:** {chat['bot']}")  
+
 
 # Footer
 st.markdown("""
