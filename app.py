52dc7da904e0f504395aae7207911a5da55e068f
juanarfuch <juan.arfuch@gmail.com>
2023-05-11 21:01:27 -0300

    Streamlit app

--- app.py
+++ app.py
@@ -1,70 +1,53 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+# Import necessary libraries
+import streamlit as st
 from utils.video_processing import load_transcript, split_transcript
-from utils.database import create_db  
+from utils.database import create_db
+from langchain.llms import OpenAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.chains import ConversationalRetrievalChain
-from langchain.memory import ConversationBufferMemory
 from langchain.chains import LLMChain
-from langchain.chat_models import ChatOpenAI
 from utils.prompts import CONDENSE_PROMPT, QA_PROMPT
-from gunicorn import  app
-import os
 
-app = Flask(__name__)
-app.secret_key = os.getenv("FLASK_SECRET_KEY", default="fallback_key")  # Use environment variable for secret key
+# Set Streamlit page configuration
+st.set_page_config(page_title='🧠ChatBot🤖', layout='wide')
 
-# Initialize outside the request functions for better performance
-chat = ChatOpenAI(temperature=0.2, model_name='gpt-3.5-turbo')
-question_generator = LLMChain(llm=chat,  prompt=CONDENSE_PROMPT)
-doc_chain = load_qa_chain(chat, prompt=QA_PROMPT, chain_type="stuff")
+# Initialize session states
+if "chat_history" not in st.session_state:
+    st.session_state["chat_history"] = []
 
-@app.route('/', methods=['GET', 'POST'])
-def home():
-    """Home route. Handles form submission for YouTube video URL."""
-    try:
-        if request.method == 'POST':
-            video_url = request.form.get('video_url')
-            session['video_url'] = video_url
-            return redirect(url_for('chat'))
-        return render_template('index.html')
-    except Exception as e:
-        # Handle exceptions and show a friendly error message
-        return render_template('error.html', message=str(e))
+# Sidebar options
+with st.sidebar.expander("🛠️ ", expanded=False):
+    # Option to start a new chat
+    if st.button("New Chat"):
+        st.session_state["chat_history"] = []
 
-@app.route('/chat', methods=['GET', 'POST'])
-def chat():
-    """Chat route. Handles form submission for user question and generates chatbot response."""
-    try:
-        if 'video_url' not in session:
-            return redirect(url_for('home'))
-
-        video_url = session['video_url']
+# Set up the Streamlit app layout
+st.title("🤖 Chat Bot with 🧠")
+st.subheader(" Powered by 🦜 LangChain + OpenAI + Streamlit")
+if st.session_state["chat_history"]==[]:
+    video_url = st.text_input("Please enter the YouTube video URL: ")
+    if video_url:
+        # Load and split transcript, create embeddings, and vectorstore
         transcript = load_transcript(video_url)
         docs = split_transcript(transcript)
         db = create_db(docs)
-        memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
-        chain = ConversationalRetrievalChain(retriever=db, question_generator=question_generator, combine_docs_chain=doc_chain)
+
+        llm = OpenAI(temperature=0.2)
+        question_generator = LLMChain(llm=llm, prompt=CONDENSE_PROMPT)
+        doc_chain = load_qa_chain(llm, prompt=QA_PROMPT)
+        chain = ConversationalRetrievalChain(
+            retriever=db,
+            question_generator=question_generator,
+            combine_docs_chain=doc_chain,
+        )
         
-        if request.method == 'POST':
-            user_question = request.form.get('user_question')
-            chat_history = session.get('chat_history', [])
-            chat_history.append(('User', user_question))
-            session['chat_history'] = chat_history
-            response = chain.run({"question": user_question, "chat_history": chat_history})
-            chat_history.append(('Chatbot', response))
-            session['chat_history'] = chat_history
+        user_question = st.text_input("Enter your question")
+        if user_question:
+            result = chain({"question": user_question, "chat_history": st.session_state["chat_history"]})
+            st.session_state["chat_history"].append((user_question, result['answer']))
 
-        chat_history = session.get('chat_history', [])
-        return render_template('chat.html', chat_history=chat_history)
-    except Exception as e:
-        # Handle exceptions and show a friendly error message
-        return render_template('error.html', message=str(e))
-
-@app.route('/delete-chat-history', methods=['POST'])
-def delete_chat_history():
-    """Delete chat history route. Clears the chat history in the session."""
-    session.pop('chat_history', None)
-    return redirect(url_for('chat'))
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+        # Display the conversation history
+        with st.expander("Conversation", expanded=True):
+            for user, bot in st.session_state["chat_history"]:
+                st.info(f'User: {user}')
+                st.success(f'Bot: {bot}')
